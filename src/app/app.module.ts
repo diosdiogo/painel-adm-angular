ccc977d30c2123286d7e9434d64c2ffca8c92406
@diogoCesar <39969711+diosdiogo@users.noreply.github.com>
2021-10-05 07:34:34 -0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/tamplate/header/header.component';
+import { FooterComponent } from './components/tamplate/footer/footer.component';
+import { NavComponent }  from './components/tamplate/nav/nav.component';
+
+import { ButtonModule, CoreModule, IconsModule, ToolbarModule, SidebarModule } from 'truly-ui'; //Import Modules
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule.forRoot({theme: 'default'}),
+    ButtonModule,
+    IconsModule,
+    ToolbarModule,
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
